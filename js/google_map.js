aa59cd6c7aaa8580aeae9d56bcb179ba3c2be528
cperez2187 <cperez2187@gmail.com>
2017-10-14 16:21:36 -0700

    Removed blog section, rearranged order and changed maps location

--- js/google_map.js
+++ js/google_map.js
@@ -4,14 +4,11 @@ var google;
 function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
+    var myLatlng = new google.maps.LatLng(33.307575, -111.844940);
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 7,
+        zoom: 9,
 
         // The latitude and longitude to center the map (always required)
         center: myLatlng,
