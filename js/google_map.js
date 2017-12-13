987fab5ecc220fe3177ebb38a506b3254dc5ae6f
cperez2187 <cperez2187@gmail.com>
2017-12-12 20:34:11 -0700

    Removed marker on map

--- js/google_map.js
+++ js/google_map.js
@@ -28,20 +28,20 @@ function init() {
     var map = new google.maps.Map(mapElement, mapOptions);
     
     // Create markers if needed
-    var addresses = ['New York City'];
+    // var addresses = ['Chandler, AZ'];
 
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
+    // for (var x = 0; x < addresses.length; x++) {
+    //     $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+    //         var p = data.results[0].geometry.location
+    //         var latlng = new google.maps.LatLng(p.lat, p.lng);
+    //         new google.maps.Marker({
+    //             position: latlng,
+    //             map: map,
+    //             icon: 'images/loc.png'
+    //         });
 
-        });
-    }
+    //     });
+    // }
     
 }
 google.maps.event.addDomListener(window, 'load', init);
